bbb48c0c7f93058fddd28e867e291a80624b892b
James McPherson <james.mcpherson@monscierge.com>
2017-10-10 15:38:40 -0500

    Added LocalizationModule as submodule

--- LocalizationDemo/LocalizationDemo/ViewController.swift
+++ LocalizationDemo/LocalizationDemo/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
