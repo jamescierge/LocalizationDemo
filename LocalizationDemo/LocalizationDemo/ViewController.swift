a00c8caf74594b65d1e62663fefdd99b7d43cb68
James McPherson <james.mcpherson@monscierge.com>
2017-10-11 10:42:34 -0500

    Pulled new changes from submod

--- LocalizationDemo/LocalizationDemo/ViewController.swift
+++ LocalizationDemo/LocalizationDemo/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        textLabel.text = "Hello, World".localized
+        textLabel.text = "Hello".localized
     }
 
     override func didReceiveMemoryWarning() {
